de92e86b48e153f89bfe15005c99dc5342dbe91e
zhangyongtian <zhangyongtianaa@sina.com>
2020-11-19 12:09:21 +0800

    添加逆向工程后的初始代码

--- remeberme/src/test/java/com/zhang/biyeseji/remeberme/RemebermeApplicationTests.java
+++ remeberme/src/test/java/com/zhang/biyeseji/remeberme/RemebermeApplicationTests.java
@@ -1,13 +1,38 @@
 package com.zhang.biyeseji.remeberme;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class RemebermeApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws InvalidConfigurationException, IOException, XMLParserException, SQLException, InterruptedException {
+		List<String> warnings = new ArrayList<String>();
+		boolean overwrite = true;
+		// 指定配置文件
+		File configFile = new File("generatorConfig.xml");
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp.parseConfiguration(configFile);
+		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+		myBatisGenerator.generate(null);
+
 	}
 
 }
